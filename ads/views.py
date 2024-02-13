87e8be70cda63d967a4d321edb96d14227067d63
uralbi <ural46@gmail.com>
2024-02-13 02:40:32 -0800

    dictionary permissions

--- ads/views.py
+++ ads/views.py
@@ -6,10 +6,23 @@ from django.http import JsonResponse
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.permissions import IsAuthenticated, AllowAny
+
 
 class DictionaryEntryViewSet(viewsets.ModelViewSet):
-    queryset = DictionaryEntry.objects.all()
+    queryset = DictionaryEntry.objects.all().order_by('-updated_at')[:40]
     serializer_class = DictionaryEntrySerializer
+
+    def get_permissions(self):
+        """
+        Instantiates and returns the list of permissions that this view requires.
+        """
+        if self.action in ['list', 'retrieve']:
+            permission_classes = [AllowAny]
+        else:
+            permission_classes = [IsAuthenticated]
+        return [permission() for permission in permission_classes]
+
 
 
 @api_view(['GET'])
