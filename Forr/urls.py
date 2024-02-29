0b05bd8d3df26fa562c70898d491e21f9cc81b2a
uralbi <ural46@gmail.com>
2024-02-28 23:51:55 -0800

    removed maitony page

--- Forr/urls.py
+++ Forr/urls.py
@@ -7,7 +7,6 @@ from django.http import HttpResponseNotFound
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls')),
-    path('ads/', include('ads.urls'))
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
